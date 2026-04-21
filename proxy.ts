771290e279a5dc8315d19ec57870b07fa6a94db2
hassanhanzala1751 <hasssanhanzala1751@gmail.com>
2026-04-21 11:34:12 -0700

    chore(proxy): Clean up whitespace in middleware file
    - Remove trailing whitespace from proxy middleware function
    - Improve code formatting consistency

--- proxy.ts
+++ proxy.ts
@@ -3,6 +3,7 @@ import { NextResponse, type NextRequest } from "next/server"
 export default async function proxy(request: NextRequest) {
   // TODO: Add authentication middleware here
   // For now, just allow all requests
+  
   return NextResponse.next()
 }
 
